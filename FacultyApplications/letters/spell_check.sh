01f95aa0596c39cf4722170180bd92264e65a216
Ryan Giordano <rgiordan@gmail.com>
2021-10-28 13:36:51 -0700

    columbia letter

--- FacultyApplications/letters/spell_check.sh
+++ FacultyApplications/letters/spell_check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #aspell --per-conf=./aspell.conf --dont-backup -t -c _letter.tex;
-aspell --per-conf=./aspell.conf --dont-backup -t -c base_letter.tex;
+aspell --per-conf=./aspell.conf --dont-backup -t -c $1;
