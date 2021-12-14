2715aa97ce5c1387f712a20879b8ef091284f6ad
Ryan Giordano <rgiordan@gmail.com>
2021-12-14 15:23:16 -0800

    cis

--- DavisInterview_20211213/cis/text.R
+++ DavisInterview_20211213/cis/text.R
@@ -46,7 +46,22 @@ estimates <-
     group_by(replicate) %>%
     summarize(mu_hat=mean(price))
 
- 
+
+alpha <- 0.9
+quantile(estimates$mu_hat, 0.9)
+quantile(estimates$mu_hat, (1 - !!alpha) / 2)
+
+estimates_quantiles <-
+    estimates %>%
+    summarize(mu_hat_med=median(mu_hat),
+              mu_lower=quantile(mu_hat, (1 - !!alpha) / 2),
+              mu_upper=quantile(mu_hat, 1 - (1 - !!alpha) / 2)) %>%
+    mutate(w_lower=mu_hat_med - mu_lower,
+           w_upper=mu_upper - mu_hat_med)
+estimates_quantiles
+w_lower <- estimates_quantiles$w_lower
+w_upper <- estimates_quantiles$w_upper
+
 PlotAxisArrow <- function(loc, height=10) {
     geom_segment(
         aes(x=!!loc, xend=!!loc, y=!!height, yend=0),
@@ -54,6 +69,7 @@ PlotAxisArrow <- function(loc, height=10) {
 }
 
 mu_hat <- quantile(estimates$mu_hat, 0.975)
+
 
 ggplot(estimates) +
     geom_histogram(aes(x=mu_hat), fill="dodgerblue3", color="lightgrey", bins=30) +
@@ -69,5 +85,35 @@ ggplot(estimates) +
     ylab("Count") + 
     xlab("Price per night (Canadian dollars)") +
     PlotAxisArrow(mu_hat, height=500)
+
+
+ggplot(estimates) +
+    geom_histogram(aes(x=mu_hat), fill="dodgerblue3", color="lightgrey", bins=30, alpha=0.1) +
+    geom_vline(aes(xintercept=mu), alpha=0.1) +
+    ylab("Count") + 
+    xlab("Price per night (Canadian dollars)") +
+    PlotAxisArrow(mu_hat, height=500)
+
+
+segments <- 
+    estimates %>%
+    filter(replicate <= 100) %>%
+    mutate(mu_lower=mu_hat - !!w_lower,
+           mu_upper=mu_hat + !!w_upper,
+           height=30 * replicate,
+           covers=((mu > mu_lower) & (mu < mu_upper)))
+sum(segments$covers)
+
+ggplot(estimates) +
+    geom_histogram(aes(x=mu_hat), fill="dodgerblue3", color="lightgrey", bins=30) +
+    geom_vline(aes(xintercept=mu)) +
+    ylab("Count") + 
+    xlab("Price per night (Canadian dollars)") +
+    geom_segment(
+        aes(x=mu_lower, xend = mu_upper, y=height, yend=height, group=replicate, color=covers),
+             arrow=arrow(ends = "both", angle = 90, length = unit(.2,"cm")),
+             data=segments)
+
+
 
 
