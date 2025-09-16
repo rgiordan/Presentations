b247c0ff2b776cfb8cc8209d79bb5aff5aa50699
Ryan Giordano <rgiordan@gmail.com>
2025-09-16 12:30:50 -0700

    load the full interactions

--- BSTARS_mrpaw_092025/figures_knitr/debugging.R
+++ BSTARS_mrpaw_092025/figures_knitr/debugging.R
@@ -14,15 +14,3 @@ source(file.path(paper_directory, "figures_knitr/define_macros.R"))
 source(file.path(paper_directory, "figures_knitr/imbalance_interaction.R"), print.eval=TRUE)
 
 
-
-imb_df %>%
-  mutate(reg_clean=case_when(reg_clean == pert_col ~ paste0(" ---> ", reg_clean),
-                             TRUE ~ reg_clean)) %>%
-  filter(interaction) %>%
-  ggplot() + 
-  geom_bar(
-    aes(fill=name, y=value, x=reg_clean),
-    position="dodge", stat="identity") +
-  # theme(axis.text.x=element_text(angle = 90, vjust = 0.5, hjust=1),
-  #       axis.ticks.x=element_blank()) +
-  coord_flip() + xlab(NULL) + ylab(NULL) + labs(fill="Method")
--- BSTARS_mrpaw_092025/figures_knitr/imbalance_interaction.R
+++ BSTARS_mrpaw_092025/figures_knitr/imbalance_interaction.R
@@ -1,8 +1,23 @@
-basic_data$imb_plot_df %>%
-  mutate(reg_clean=case_when(
-    reg_clean == basic_data$pert_col_clean ~ paste0(" ---> ", reg_clean),
-    TRUE ~ reg_clean)) %>%
+bad_regs <- basic_data$basic_metrics$imb_df %>%
+  mutate(mrp_imbalance=mrp_x - poststrat_x) %>%
   filter(interaction) %>%
+  arrange(abs(mrp_imbalance)) %>%
+  tail(10) %>%
+  pull(reg)
+
+bad_regs <- union(bad_regs, sim_data$col_pert)
+
+interaction_imb_df <-
+  basic_data$imb_plot_df %>%
+  filter(interaction) %>%
+  filter(reg %in% bad_regs) %>%
+  mutate(reg_clean=case_when(
+      reg_clean == basic_data$pert_col_clean ~ paste0(" ---> ", reg_clean),
+      TRUE ~ reg_clean))
+
+
+
+interaction_imb_df %>%
   ggplot() + 
   geom_bar(
     aes(fill=name, y=value, x=reg_clean),
