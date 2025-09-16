051055e686b8f82b163c30fc896c2eb67afd9fab
Ryan Giordano <rgiordan@gmail.com>
2025-09-15 18:21:47 -0700

    more basic data

--- BSTARS_mrpaw_092025/figures_knitr/debugging.R
+++ BSTARS_mrpaw_092025/figures_knitr/debugging.R
@@ -11,13 +11,4 @@ source(file.path(paper_directory, "figures_knitr/load_data.R"))
 source(file.path(paper_directory, "figures_knitr/define_macros.R"))
 source(file.path(paper_directory, "figures_knitr/predictions_plot.R"), print.eval=TRUE)
 
-# Define LaTeX macros that will let us automatically refer
-# to simulation and model parameters.
-DefineMacro <- function(macro_name, value, digits=3) {
-  #sprintf_code <- paste("%0.", digits, "f", sep="")
-  value_string <- format(value, big.mark=",", digits=digits, scientific=FALSE)
-  cat("\\newcommand{\\", macro_name, "}{", value_string, "}\n", sep="")
-}
-
-
-
+sim_data$imb_df
