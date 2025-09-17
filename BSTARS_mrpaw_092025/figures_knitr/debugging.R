c4f0a3fd643960f5c20c33a126357dbf8ad0665e
Ryan Giordano <rgiordan@gmail.com>
2025-09-16 19:48:05 -0700

    basic idaee

--- BSTARS_mrpaw_092025/figures_knitr/debugging.R
+++ BSTARS_mrpaw_092025/figures_knitr/debugging.R
@@ -12,9 +12,19 @@ source(file.path(paper_directory, "figures_knitr/define_macros.R"))
 #source(file.path(paper_directory, "figures_knitr/predictions_plot.R"), print.eval=TRUE)
 #source(file.path(paper_directory, "figures_knitr/imbalance_primary.R"), print.eval=TRUE)
 #source(file.path(paper_directory, "figures_knitr/imbalance_interaction.R"), print.eval=TRUE)
+source(file.path(paper_directory, "figures_knitr/weights_plot.R"), print.eval=TRUE)
 
-source(file.path(paper_directory, "figures_knitr/predictions_plot.R"), print.eval=TRUE)
+head(basic_metrics$weight_df)
 
+plt <- 
+  ggplot(basic_metrics$weight_df) +
+  geom_point(aes(x=w_mrpaw, y=w_raking * length(w_raking)), alpha=0.2) +
+  geom_abline() +
+  geom_vline(aes(xintercept=0)) +
+  xlab(TeX("$w^{MRP}_i$")) +
+  ylab(TeX("$w^{CAL}_i$"))
+
+plt
 
 as.numeric(refit_data$mean_mrpaw_time, units = "secs")
 
