8905409d58686a5f9e77d3471069b3bd4de9b839
Ryan Giordano <rgiordan@gmail.com>
2025-09-16 20:33:23 -0700

    remove binary stuff

--- BSTARS_mrpaw_092025/figures_knitr/debugging.R
+++ BSTARS_mrpaw_092025/figures_knitr/debugging.R
@@ -9,22 +9,10 @@ setwd(base_dir)
 source(file.path(base_dir, "figures_knitr/initialize.R"))
 source(file.path(paper_directory, "figures_knitr/load_data.R"))
 source(file.path(paper_directory, "figures_knitr/define_macros.R"))
-#source(file.path(paper_directory, "figures_knitr/predictions_plot.R"), print.eval=TRUE)
+source(file.path(paper_directory, "figures_knitr/predictions_plot.R"), print.eval=TRUE)
+
 #source(file.path(paper_directory, "figures_knitr/imbalance_primary.R"), print.eval=TRUE)
 #source(file.path(paper_directory, "figures_knitr/imbalance_interaction.R"), print.eval=TRUE)
-source(file.path(paper_directory, "figures_knitr/weights_plot.R"), print.eval=TRUE)
+#source(file.path(paper_directory, "figures_knitr/weights_plot.R"), print.eval=TRUE)
 
-head(basic_metrics$weight_df)
-
-plt <- 
-  ggplot(basic_metrics$weight_df) +
-  geom_point(aes(x=w_mrpaw, y=w_raking * length(w_raking)), alpha=0.2) +
-  geom_abline() +
-  geom_vline(aes(xintercept=0)) +
-  xlab(TeX("$w^{MRP}_i$")) +
-  ylab(TeX("$w^{CAL}_i$"))
-
-plt
-
-as.numeric(refit_data$mean_mrpaw_time, units = "secs")
 
