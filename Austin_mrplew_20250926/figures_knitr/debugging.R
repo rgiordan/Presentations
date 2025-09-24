5c884a75d6b1e2aa1ac70171e9769c506d47e871
Ryan Giordano <rgiordan@gmail.com>
2025-09-24 13:55:38 -0700

    bootstrap

--- Austin_mrplew_20250926/figures_knitr/debugging.R
+++ Austin_mrplew_20250926/figures_knitr/debugging.R
@@ -13,7 +13,9 @@ source(file.path(paper_directory, "figures_knitr/define_macros.R"))
 source("figures_knitr/predictions_plot.R")
 source("figures_knitr/imbalance_plots.R")
 
-#source(file.path(paper_directory, "figures_knitr/weights_plot.R"), print.eval=TRUE)
+source(file.path(paper_directory, "figures_knitr/weights_plot.R"), print.eval=TRUE)
+
+#lax_weight_scatterplot
 
 # source("figures_knitr/predictions_plot.R")
 # alex_refit_plots$base_plot
@@ -37,73 +39,6 @@ source("figures_knitr/imbalance_plots.R")
 
 
 analysis_list <- alex
-
-make_imbalance_plots <- function(analysis_list) {
-  basic_data  <- analysis_list$basic_data
-  sim_data <- analysis_list$sim_data
-  
-  imb_plot_df <-
-    basic_data$imb_plot_df %>%
-    pivot_wider(id_cols=c(reg, interaction, reg_clean), 
-                names_from=name, values_from=value) %>%
-    mutate(MRP=MRP - Target, Raking=Raking - Target) %>%
-    pivot_longer(c(MRP, Target, Raking)) %>%
-    mutate(value_pct=100 * value / basic_data$basic_metrics$mrp) %>%
-    mutate(reg_clean=case_when(
-      reg_clean == basic_data$pert_col_clean ~ paste0(" ---> ", reg_clean),
-      TRUE ~ reg_clean))
-  
-  
-  # Jitter so the zeros are visible
-  jitter_amount <- max(abs(imb_plot_df$value_pct)) * 5e-4
-  primary_plt <-
-    imb_plot_df %>%
-    filter(!interaction, name != "Target") %>%
-    ggplot() +
-    geom_bar(
-      aes(fill=name, y=value_pct + jitter_amount, x=reg_clean),
-      position="dodge", stat="identity") +
-    coord_flip() + 
-    xlab(NULL) + 
-    labs(fill="Method") + 
-    ylab("Imbalance (% of MrP estimate)")
-  
-  #primary_plt
-  
-  bad_regs <- basic_data$basic_metrics$imb_df %>%
-    mutate(mrp_imbalance=mrp_x - poststrat_x) %>%
-    filter(interaction) %>%
-    arrange(abs(mrp_imbalance)) %>%
-    tail(10) %>%
-    pull(reg)
-  
-  #bad_regs <- union(bad_regs)
-  
-  interaction_imb_df <-
-    imb_plot_df %>%
-    filter(interaction, name != "Target") %>%
-    filter(reg %in% bad_regs)
-  
-  
-  interaction_plt <-
-    interaction_imb_df %>%
-    ggplot() +
-    geom_bar(
-      aes(fill=name, y=value_pct, x=reg_clean),
-      position="dodge", stat="identity") +
-    coord_flip() + 
-    xlab(NULL) + 
-    labs(fill="Method") + 
-    ylab("Imbalance (% of MrP estimate)")
-  
-  #interaction_plt
-  
-  return(list(interaction_plt=interaction_plt, primary_plt=primary_plt))
-}
-
-
-alex_imb_plots <- make_imbalance_plots(alex)
-lax_imb_plots <- make_imbalance_plots(lax)
 
 lax_imb_plots$primary_plt
 alex_imb_plots$primary_plt
--- Austin_mrplew_20250926/figures_knitr/weights_plot.R
+++ Austin_mrplew_20250926/figures_knitr/weights_plot.R
@@ -1,9 +1,16 @@
+make_weight_scatterplot <- function(analysis_list) {
+  plt <-
+    ggplot(analysis_list$basic_data$basic_metrics$weight_df) +
+    geom_point(aes(x=w_mrpaw, y=w_raking * length(w_raking)), alpha=0.2) +
+    geom_abline() +
+    geom_vline(aes(xintercept=0)) +
+    xlab(TeX("$w^{MRP}_i$")) +
+    ylab(TeX("$w^{CAL}_i$"))
+  return(plt)
+}
 
-plt <- 
-  ggplot(alex$basic_data$basic_metrics$weight_df) +
-  geom_point(aes(x=w_mrpaw, y=w_raking * length(w_raking)), alpha=0.2) +
-  geom_abline() +
-  geom_vline(aes(xintercept=0)) +
-  xlab(TeX("$w^{MRP}_i$")) +
-  ylab(TeX("$w^{CAL}_i$"))
-print(plt)
+
+alex_weight_scatterplot <- make_weight_scatterplot(alex)
+lax_weight_scatterplot <- make_weight_scatterplot(lax)
+
+
