16b2517c5b4f8604340da94c51289c8f79be26fd
Ryan Giordano <rgiordan@gmail.com>
2025-10-21 12:03:47 -0700

    new intro

--- Stanford_mrplew_20251021/figures_knitr/debugging.R
+++ Stanford_mrplew_20251021/figures_knitr/debugging.R
@@ -10,6 +10,23 @@ source(file.path(base_dir, "figures_knitr/initialize.R"))
 source(file.path(paper_directory, "figures_knitr/load_data.R"))
 source(file.path(paper_directory, "figures_knitr/define_macros.R"))
 
-source(file.path(paper_directory, "figures_knitr/weights_plot.R"))
+source(file.path(paper_directory, "figures_knitr/simulation_fig.R"))
 
+
+#source(file.path(paper_directory, "figures_knitr/weights_plot.R"))
+
+df <- data.frame(
+  x_start=1,
+  x_end=2,
+  y_start=1,
+  y_end=2
+)
+ggplot(df, aes(x= x_start, y = y_start, xend = x_end, yend = y_end)) +
+  geom_point(aes(x = x_start, y = y_start), color = "blue", size = 3) + # Start points
+  geom_point(aes(x = x_end, y = y_end), color = "blue", size = 3) +     # End points
+  geom_segment(arrow = arrow(length = unit(0.5, "cm")), color="blue") +             # Draw arrows
+  labs(title = "Arrows between points", x =TeX("$y_1$"), y =TeX("$y_2")) +
+  geom_label(aes(x=x_start, y=y_start, label=TeX("$Y_S$"))) +
+  theme_minimal()  +
+  xlim(0.5, 2.5) + ylim(0.5, 2.5)
 
--- Stanford_mrplew_20251021/figures_knitr/simulation_fig.R
+++ Stanford_mrplew_20251021/figures_knitr/simulation_fig.R
@@ -1,14 +1,16 @@
 
 
 pred_df <- sim_env$pred_df
-
+x <- sim_env$x
+ylab_text <- sprintf("x = %0.1f", x)
 print(
 ggplot(pred_df) +
     geom_line(aes(x=delta, y=yhat_glm, color="Logistic regression")) +
     geom_line(aes(x=delta, y=yhat_ols, color="Linear regression")) +
-    ylab(TeX("$\\hat{y}(\\delta)$  for $x = 0.5$")) +
+    ylab(TeX("$\\hat{y}(\\delta)$")) +
     xlab(TeX("$\\delta")) +
-    theme(legend.title = element_blank())
+    theme(legend.title = element_blank()) +
+    ggtitle(sprintf("Predictions at x = %0.1f", sim_env$x))
 )
 
 # grid.arrange(
