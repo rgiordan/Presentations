d12f8c8c2725d71b72985436c647db32064fe9fa
Ryan Giordano <rgiordan@gmail.com>
2026-08-18 17:21:51 -0700

    diagnostic section

--- Berkeley_Midcareer_2026/R_scripts/debugging.R
+++ Berkeley_Midcareer_2026/R_scripts/debugging.R
@@ -14,9 +14,34 @@ source(file.path(base_dir, "R_scripts/logistic_otto_sim/load_data.R"))
 source(file.path(base_dir, "R_scripts/logistic_otto_sim/generate_histogram_plots.R"))
 
 
-source(file.path(base_dir, "R_scripts/logistic_otto_sim/generate_histogram_plots.R"))
-
-plot0 +
-  scale_y_continuous(breaks = seq(from = 0, to = 30, by = 10), limits=c(0, 35)) 
+source(file.path(base_dir, "R_scripts/logistic_otto_sim/generate_shift_plots.R"))
 
 
+shift_base_plt + survey_hist + correct_reg
+
+shift_base_plt + survey_hist + correct_reg
+
+shift_base_plt + survey_hist + misspec_reg + correct_reg
+
+shift_base_plt + survey_hist + misspec_reg + correct_reg + x2_zone
+
+
+
+source(file.path(base_dir, "R_scripts/logistic_otto_sim/generate_shift_plots.R"))
+survey_shifted_hist <-
+  geom_histogram(aes(x=.data[[col]], 
+                     y=..density.. / max(..density..), 
+                     fill="shifted survey"), 
+                 data=survey_df %>% filter(X2 > x2_thresh),
+                 alpha=1.0)
+
+misspec_shifted_reg <-
+  geom_smooth(aes(x=.data[[col]], y=yhat1 + 0.02 * (.data[[col]] - mean(.data[[col]])), 
+                  linetype="Misspecification (shifted)"),
+              data=survey_df, se = FALSE)
+
+shift_base_plt + survey_hist + poststrat_hist
+shift_base_plt + survey_hist + correct_reg
+shift_base_plt + survey_hist + misspec_reg + correct_reg
+shift_base_plt + survey_shifted_hist + survey_hist + misspec_reg + correct_reg + misspec_shifted_reg
+shift_base_plt + survey_shifted_hist + survey_hist + misspec_reg + correct_reg + misspec_shifted_reg
--- Berkeley_Midcareer_2026/R_scripts/logistic_otto_sim/generate_shift_plots.R
+++ Berkeley_Midcareer_2026/R_scripts/logistic_otto_sim/generate_shift_plots.R
@@ -2,23 +2,43 @@
 survey_df <- logistic_env$survey_df
 poststrat_df <- logistic_env$poststrat_df
 
-col <- "X2"
-shift_plot0 <- 
-ggplot() +
-  geom_histogram(aes(x=.data[[col]], y=..density../ max(..density..), fill="survey"), 
-                 data=survey_df, alpha=0.5) +
-  geom_histogram(aes(x=.data[[col]], y=..density.. / max(..density..), fill="poststrat"), 
-                 data=poststrat_df, alpha=0.5) +
-  geom_smooth(aes(x=.data[[col]], y=yhat0, linetype="Correct specification"), data=survey_df) +
-  geom_smooth(aes(x=.data[[col]], y=yhat1, linetype="Misspecification"), data=survey_df) +
-  xlab(col) + ylab("") +
-  scale_linetype(name="Model") +
-  scale_fill_discrete(name="Dataset",
-                      breaks=c("poststrat", "survey"),
-                      labels=c("Target population",
-                               "Survey population\n(used for estimation)"))
 
-shift_plot1 <- 
-  shift_plot0 +
-  annotate("rect", xmin = 0.878, xmax = Inf, ymin = 0, ymax = 0.05, 
+
+
+col <- "X2"
+
+survey_hist <-
+  geom_histogram(aes(x=.data[[col]], y=..density../ max(..density..), fill="survey"), 
+                 data=survey_df, alpha=1.0)
+poststrat_hist <-
+  geom_histogram(aes(x=.data[[col]], y=..density.. / max(..density..), fill="poststrat"), 
+                 data=poststrat_df, alpha=0.5)
+
+correct_reg <-
+  geom_smooth(aes(x=.data[[col]], y=yhat0, linetype="Correct specification"), 
+  data=survey_df, se = FALSE)
+
+misspec_reg <-
+  geom_smooth(aes(x=.data[[col]], y=yhat1, linetype="Misspecification"),
+  data=survey_df, se = FALSE)
+
+x2_thresh <- 0.878
+x2_zone <-
+  annotate("rect", xmin = x2_thresh, xmax = Inf, ymin = 0, ymax = 0.05, 
          fill = "purple", alpha = 0.5)
+
+shift_base_plt <-
+  ggplot() +
+    xlab(col) + ylab("") +
+    scale_linetype(name="Regression model") +
+    scale_fill_manual(name="Dataset",
+                        breaks=c("survey", "shifted survey", "poststrat"),
+                        labels=c("Survey population\n(used for estimation)",
+                                "Survey population (shifted)",
+                                "Target population"),
+                        values=c("orange", "salmon", "dark green")) +
+    scale_y_continuous(breaks = seq(from = 0, to = 1, by = 0.25), limits=c(0, 1)) +
+    scale_x_continuous(breaks = seq(from = -0.5, to = 2.5, by = 0.5), limits=c(-0.3, 2.4))
+
+
+
